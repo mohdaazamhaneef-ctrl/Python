7028bd420149e7a170c29747b7ee2dd733bacad5
Mohd Aazam Haneef <mohdaazamhaneef@gmail.com>
2026-09-20 18:44:31 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -20,4 +20,28 @@ print(data)
 # Tuple is immutable
 # A Tuple is immutable, which means its elements cannot be changed, update or deleted after creation.
 data = (10,20,30,40,50)
-data[0]=80              #TypeError: 'tuple' object does not support item assignment
+#data[0]=80              #TypeError: 'tuple' object does not support item assignment
+
+# Tuple Packing
+# Ex- 1
+studentData = "Aazam", "MCA", 78.65
+print(type(studentData)) 
+
+# Ex- 2
+record = "Aazam", "MCA", 23, True
+print(record[0])
+print([record[1]])
+print(record)
+
+# Ex- 3
+record = "Aazam", "MCA", 23, True
+name,*ab,att = record
+print(name)
+print(ab)
+print(att)
+
+# Tuple Unpacking
+data = "Aazam", "MCA", 23, True
+n,*ab = data
+print(n)
+print(ab)
